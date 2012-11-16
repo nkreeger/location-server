ee4857a4f025c95266efdd37c1c5952610697af9
Nick Kreeger <kreeger@strava.com>
2012-11-16 14:35:42 -0800

    Server demo time.

--- server.js
+++ server.js
@@ -12,35 +12,119 @@ var HOST = "192.168.2.1";  // HACK FOR NOW
 //var HOST = "127.0.0.1";
 var PORT = 5001;
 
-//==================================================================================================
 //
-// Reported rides summary
+// Location check-in database
 //
 
+var Location = null;
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost', 'test');
+db.once('open', function() {
+    var LocationSchema = new mongoose.Schema({
+        athleteId: Number, 
+        longitude: Number,
+        latitude: Number,
+        time: Number
+    });
+    Location = db.model('Location', LocationSchema);
+});
 
-//==================================================================================================
+//
+// Misc. helper methods
+//
+function jsonReplacer(key, value) {
+    if (key == "_id") return undefined;
+    if (key == "__v") return undefined;
+    return value;
+}
+
+function log(message) {
+    console.log("---------------------------");
+    console.log(message);
+    console.log("---------------------------");
+}
+
 //
 // Location TCP socket thingy
 //
+
+var connectedSockets = [];
+
+function socketQuery(socket, query) {
+    Location.find(query, function(err, locations) {
+        log("sending athlete socket(" + socket.athleteId + ") for query: '" + JSON.stringify(query) + "'" + 
+        "result: " + locations.length);
+
+        var update = {
+            request: "athletes_near",
+            athletes: []
+        };
+
+        for (var key in locations) {
+            var curLocation = locations[key];
+
+            // TODO: Filter on location!
+            // TODO: Filter on time!
+            // TODO: Filter duplicates!
+
+            update.athletes.push(curLocation.athleteId);
+        }
+
+        try {
+            socket.write(JSON.stringify(update));
+        } catch (e) { 
+        }
+    });
+}
+
+
+function notifyConnectedSockets() {
+    for (var i = 0; i < connectedSockets.length; i++) {
+        var curSocket = connectedSockets[i];
+        var query = {
+            $where : "this.athleteId != " + curSocket.athleteId
+        };
+
+        socketQuery(curSocket.socket, query);
+    }
+}
+
 var server = net.createServer();
 server.listen(PORT, HOST);
 server.on('connection', function(socket) {
     console.log("CONNECTED: " + socket.remoteAddress +  ":" + socket.remotePort);
 
-    socket.on('connect', function(data) {
-        console.log(">>> CONNECTION ESTABLISHED");
+    socket.on('connect', function() {
+        log("Connection: (connections: " + server.connections + ")");
     });
 
     socket.on('data', function(data) {
         console.log("DATA: " + socket.remoteAddress + ": " + data + ", type: " + typeof(data));
-        socket.write("You said '" + data + "'");
 
-        // Thankfully 'data' is already a JSON object... now just do something with it.
-
+        var message = JSON.parse(data);
+        if (message.request == "hello") {
+            connectedSockets.push({
+                "socket" : socket,
+                "athleteId" : message.athleteId
+            });
+        }
+        else if (message.request == "report") {
+            new Location(message.report).save();
+            notifyConnectedSockets();
+        }
     });
 
     socket.on('close', function(data) {
-        console.log("CLOSED: " + socket.remoteAddress + ":" + socket.remotePort);
+        console.log("CLOSED: " + data);
+
+        // Prob not great?
+        //for (var key in connectedSockets) {
+        //    var curSocket = connectedSockets[key];
+        //    if (curSocket == socket) {
+        //        console.log(">>>>>> FOUND SOCKET");
+        //        // REMOVE ITEM!!
+        //    }
+        //}
     });
 });
 
