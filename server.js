dd90343e9eb9ef0e7ab91e13096bd7537cc1110d
Nick Kreeger <kreeger@strava.com>
2012-11-20 11:03:37 -0800

    parking wip

--- server.js
+++ server.js
@@ -85,102 +85,106 @@ function postMessage(message, ignoreSocketGuid) {
 // Location TCP socket thingy
 //
 var server = net.createServer();
-server.listen(PORT);
-server.on('connection', function(socket) {
-    log("Connection Started: " + socket.remoteAddress +  ":" + socket.remotePort);
-
-    socket.on('connect', function() {
-        socket.guid = newGuid();
-        activeConnections[socket.guid] = {
-            socket: socket,
-            hasSaidHello: false,
-            athleteId: -1
-        };
-
-        for (var key in activeConnections) {
-            console.log("key: " + key);
-        }
-
-        socket.write(JSON.stringify({ request: "hello" }));
-
-        log("Socket Connected [" + socket.guid + "] : (connections: " + server.connections + ")");
-    });
-
-    socket.on('data', function(data) {
-        log("Socket Data [" + socket.guid + "] : " + data);
-
-        var message = JSON.parse(data);
-        if (message.request == "hello") {
-            // Set the athlete ID associated with the socket.
-            socket.athleteId = message.athleteId;
-
-//          Handy for debugging:
-//            // Inform the new athlete about other connected athletes
-//            {
-//                var message = JSON.stringify({
-//                    request: "connected_clients",
-//                    clients: athleteLocations
-//                });
-//                socket.write(message);
-//            }
-//            // Inform other connected athletes that a new athlete has joined
-//            {
-//                var message = JSON.stringify({
-//                    request: "client_joined",
-//                    athleteId: message.athleteId
-//                });
-//                postMessage(message, socket.guid);
-//            }
-        } else if (message.request == "location_report") {
-            // Store the variables in app-session for now. Would be nice to keep one record
-            // on disk - not sure if mongodb is great for that.
-
-            athleteLocations[message.athleteId] = {
-                latitude: message.latitude,
-                longitude: message.longitude
+server.listen(PORT, function() {
+    console.log(">>>>>>>>> Lisetning on port: " + PORT);
+    server.on('connection', function(socket) {
+        log("Connection Started: " + socket.remoteAddress +  ":" + socket.remotePort);
+    
+        socket.on('connect', function() {
+            socket.guid = newGuid();
+            activeConnections[socket.guid] = {
+                socket: socket,
+                hasSaidHello: false,
+                athleteId: -1
             };
-            
-            // Inform other connected athletes of new athlete.
-            var message = JSON.stringify({
-                request: "close_athletes",
-                athleteId: message.athleteId,
-                latitude: message.latitude,
-                longitude: message.longitude
-            });
-            postMessage(message, socket.guid);
-
-            // Inform the current athlete about other connected athletes
-            // NOTE: This is a hack that just appends all connected athletes.
-            var closeAthletes = {};
-            for (var athleteId in athleteLocations) {
-                if (athleteId != socket.athleteId) {
-                    closeAthletes[athleteId] = athleteLocations[athleteId];
-                }
+    
+            for (var key in activeConnections) {
+                console.log("key: " + key);
             }
-            message = JSON.stringify({
-                request: "close_athletes",
-                clients: closeAthletes
-            });
-            socket.write(message);
-            
-        }
-    });
-
-    socket.on('close', function(data) {
-        delete activeConnections[socket.guid];
-        delete athleteLocations[socket.athleteId];
-
-        log("Socket Closed [" + socket.guid + "] : (connections: " + server.connections + ")");
-
-        // Report:
-        var message = JSON.stringify({
-            request: "client_left",
-            athleteId: socket.athleteId
+    
+            socket.write(JSON.stringify({ request: "hello" }));
+    
+            log("Socket Connected [" + socket.guid + "] : (connections: " + server.connections + ")");
         });
-        for (var key in activeConnections) {
-            activeConnections[key].socket.write(message);
-        }
+    
+        socket.on('data', function(data) {
+            log("Socket Data [" + socket.guid + "] : " + data);
+    
+            var message = JSON.parse(data);
+            if (message.request == "hello") {
+                // Set the athlete ID associated with the socket.
+                socket.athleteId = message.athleteId;
+    
+    //          Handy for debugging:
+    //            // Inform the new athlete about other connected athletes
+    //            {
+    //                var message = JSON.stringify({
+    //                    request: "connected_clients",
+    //                    clients: athleteLocations
+    //                });
+    //                socket.write(message);
+    //            }
+    //            // Inform other connected athletes that a new athlete has joined
+    //            {
+    //                var message = JSON.stringify({
+    //                    request: "client_joined",
+    //                    athleteId: message.athleteId
+    //                });
+    //                postMessage(message, socket.guid);
+    //            }
+            } else if (message.request == "location_report") {
+                // Store the variables in app-session for now. Would be nice to keep one record
+                // on disk - not sure if mongodb is great for that.
+    
+                athleteLocations[message.athleteId] = {
+                    latitude: message.latitude,
+                    longitude: message.longitude
+                };
+                
+                // Inform other connected athletes of new athlete.
+                var message = JSON.stringify({
+                    request: "close_athletes",
+                    athleteId: message.athleteId,
+                    latitude: message.latitude,
+                    longitude: message.longitude
+                });
+                postMessage(message, socket.guid);
+    
+                // Inform the current athlete about other connected athletes
+                // NOTE: This is a hack that just appends all connected athletes.
+                var closeAthletes = {};
+                for (var athleteId in athleteLocations) {
+                    if (athleteId != socket.athleteId) {
+                        closeAthletes[athleteId] = athleteLocations[athleteId];
+                    }
+                }
+                message = JSON.stringify({
+                    request: "close_athletes",
+                    clients: closeAthletes
+                });
+                socket.write(message);
+                
+            }
+        });
+    
+        socket.on('close', function(data) {
+            delete activeConnections[socket.guid];
+            delete athleteLocations[socket.athleteId];
+    
+            log("Socket Closed [" + socket.guid + "] : (connections: " + server.connections + ")");
+    
+            // Report:
+            var message = JSON.stringify({
+                request: "client_left",
+                athleteId: socket.athleteId
+            });
+            for (var key in activeConnections) {
+                activeConnections[key].socket.write(message);
+            }
+        });
     });
+
+
 });
 //console.log("Server listening on " + HOST + ":" + PORT);
 
