7c7f9132c7327b1bc2ad6eef401d52cf23be7dbf
Nick Kreeger <kreeger@strava.com>
2012-11-21 19:10:36 -0700

    Parking wip now that the server is communicating with each other.

--- new-server.js
+++ new-server.js
@@ -21,12 +21,31 @@ app.listen(process.env.PORT || 5001);
 
 var io = require('socket.io').listen(app);
 
-var locations = io.of('/locations');
-locations.on('connection', function(socket) {
-    socket.emit("hello", { message: "This is a test message" });
+io.sockets.on('connection', function(socket) {
+    socket.emit('hello');
 
     socket.on('hello', function(data) {
-        console.log(data);
+        console.log(">>>> GOT HELLO: ", data);
     });
 });
 
+
+/*
+63 var io = require('socket.io').listen(80);
+264 
+265 io.sockets.on('connection', function (socket) {
+266   socket.broadcast.emit('user connected');
+267   socket.broadcast.json.send({ a: 'message' });
+268 });
+*/
+
+// Kinda old
+//var locations = io.of('/locations');
+//locations.on('connection', function(socket) {
+//    socket.emit("hello", { message: "This is a test message" });
+//
+//    socket.on('hello', function(data) {
+//        console.log(data);
+//    });
+//});
+//
