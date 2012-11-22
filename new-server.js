8ebe0dd990c09430d88b912450c5453c17b1276b
Nick Kreeger <kreeger@strava.com>
2012-11-22 11:52:53 -0600

    Heroku bump

--- new-server.js
+++ new-server.js
@@ -39,23 +39,3 @@ io.sockets.on('connection', function(socket) {
     });
 });
 
-
-/*
-63 var io = require('socket.io').listen(80);
-264 
-265 io.sockets.on('connection', function (socket) {
-266   socket.broadcast.emit('user connected');
-267   socket.broadcast.json.send({ a: 'message' });
-268 });
-*/
-
-// Kinda old
-//var locations = io.of('/locations');
-//locations.on('connection', function(socket) {
-//    socket.emit("hello", { message: "This is a test message" });
-//
-//    socket.on('hello', function(data) {
-//        console.log(data);
-//    });
-//});
-//
